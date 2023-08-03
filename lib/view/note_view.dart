91d171236708d55690f589bf7f57a5bdd9bbd4e9
abdulmalek9 <akabdalmalek@gmail.com>
2023-08-03 15:34:43 +0200

    Added NotesViewBody widget.

--- lib/view/note_view.dart
+++ lib/view/note_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:note/view/widget/list_of_notes.dart';
 import 'package:note/view/widget/notes_app_bar.dart';
 import 'package:note/view/widget/notes_card.dart';
+import 'package:note/view/widget/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -11,21 +12,7 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 18,
-            ),
-            NotesViewAppBar(),
-            SizedBox(
-              height: 24,
-            ),
-            Expanded(child: ListOfNotes()),
-          ],
-        ),
-      ),
+      body: NotesViewBody(),
     );
   }
 }
