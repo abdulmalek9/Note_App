89362bb77221f1be4a6d89334bbdde428ab67192
abdulmalek9 <akabdalmalek@gmail.com>
2023-08-15 15:08:30 +0200

    hive.open box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note/view/constent.dart';
 import 'package:note/view/note_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
 }
 
