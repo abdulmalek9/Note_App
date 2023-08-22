3b3b0aacb96e89794dfe3f42bbe624e7d12d7404
abdulmalek9 <akabdalmalek@gmail.com>
2023-08-22 09:57:35 +0200

    Update dependencies and add bloc observer. (generated by useblackbox.ai )

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note/Cubit/add_notes_cubit/add_note_cubit.dart';
 import 'package:note/models/note_model.dart';
+import 'package:note/simple_bloc_observer.dart';
 import 'package:note/view/constent.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note/view/note_view.dart';
 
 void main() async {
   await Hive.initFlutter();
-
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NoteApp());
